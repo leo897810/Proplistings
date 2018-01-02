cc1d3cc9a3de60e5dd148aa9b24d6d2876bda0ab
Shengkai Li <099715@gmail.com>
2018-01-02 18:58:26 +1100

    set up and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListingsComponent } from './listings/listings.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ListingComponent } from './listing/listing.component';
+import { AddListingComponent } from './add-listing/add-listing.component';
+import { EditListingComponent } from './edit-listing/edit-listing.component';
 
+const appRoutes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'listings', component:ListingsComponent},
+  {path:'item', component:ListingComponent},
+  {path:'add-listing', component:AddListingComponent},
+  {path:'edit', component:EditListingComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListingsComponent,
+    NavbarComponent,
+    ListingComponent,
+    AddListingComponent,
+    EditListingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
